40cc8350824e3509cdae5b53ca9302197cabdcc0
Z User <z@container>
2026-04-17 21:31:45 +0000

    df55265b-a83c-4c0b-a7e9-6eb7f6dda8fa

--- run-server.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-echo "Iniciando servidor Next.js..."
-exec node node_modules/.bin/next dev -p 3000
--- start-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /home/z/my-project
-while true; do
-    echo "Iniciando servidor..."
-    bun run dev 2>&1 | tee dev.log
-    echo "Servidor detenido. Reiniciando en 3 segundos..."
-    sleep 3
-done
